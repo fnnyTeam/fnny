be97b11a8519b143a385839456dd28abc01853cc
张东瀛 <534501706@qq.com>
2017-06-07 15:38:44 +0800

    choose submit first

--- yhouse/src/pages/food/food.vue
+++ yhouse/src/pages/food/food.vue
@@ -1,10 +1,37 @@
 <template>
 	<div class="food">
 		<swiper :swiperdata = "swiperData"></swiper>
-		
+		<div class="food_banner">
+			<div class="food_bannerPic" v-for="item in bannerData">
+				<a href=""><img :src="item.picUrl" alt=""></a>
+			</div>
+		</div>
 	</div>	
 </template>
-
+<style type="text/css">
+	.food_banner{
+		display: flex;
+		justify-content: space-around;
+		flex-wrap: wrap;
+		width: 100%;
+		height: 1.5rem;
+		margin-bottom: 0.099638rem;
+		
+	}
+	.food_bannerPic{
+		width: 48.5%;
+		height: 46%;
+	}
+	.food_bannerPic a{
+		display: block;
+		width: 100%;
+		height: 100%;
+	}
+	.food_bannerPic img{
+		width: 100%;
+		height: 100%;
+	}
+</style>
 <script type="text/javascript">
 import Swiper from './../../components/swiper/swiper'
 export default{
@@ -15,16 +42,20 @@ export default{
 	},
 	data () {
 		return {
-			swiperData: []
+			swiperData: [],
+			bannerData: []
 		}
 	},
 	created () {
 		this.axios.get('static/data/data/food/jxtheme.json').then(res => {
 			this.swiperData = res.data.data.reserveList[0].content
-			console.log(this.swiperData)
+			this.bannerData = res.data.data.bundle[0].content
+			console.log(this.bannerData)
+			
 		},err =>{
 			console.log(err)
 		},'json')
+		
 	}
 }
 
--- yhouse/src/pages/home.vue
+++ yhouse/src/pages/home.vue
@@ -6,8 +6,8 @@
         <router-link to='/choose'>choose</router-link>
         <div class="tabbar">
           <router-link to='/food'>food</router-link>
-          <router-link to='/life'>life</router-link>
           <router-link to='/play'>play</router-link>
+          <router-link to='/life'>life</router-link>
           <router-link to='/spa'>spa</router-link>    
         </div>
       </div>
