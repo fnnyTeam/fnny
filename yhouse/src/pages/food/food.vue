f21f4faa7edf500dc107df2efb833e30959e9c4d
htmlfn <328770466@qq.com>
2017-06-09 14:22:47 +0800

    'friday2'

--- yhouse/src/pages/food/food.vue
+++ yhouse/src/pages/food/food.vue
@@ -1,9 +1,6 @@
 <template>
 	<div class="food">
 		<swiper :swiperdata = "swiperData"></swiper>
-		<jx-theme :jxThemeData = "jxThemeData"></jx-theme>
-		<shop-list :shopData="shopData"></shop-list>
-		<my-footer></my-footer>
 		<div class="food_banner">
 			<div class="food_bannerPic" v-for="item in bannerData">
 				<a href=""><img :src="item.picUrl" alt=""></a>
@@ -11,9 +8,11 @@
 		</div>
 		<jx-theme :jxThemeData = "jxThemeData"></jx-theme>
 		<shop-list :shopData="shopData"></shop-list>
-		<div class="load_more unload">查看更多</div>
+		<div class="play_last">
+			<p class="load_more unload">查看更多</p>
+			<!-- <p class="load_more loading">加载中</p> -->
+		</div>
 		<my-footer></my-footer>
-
 	</div>	
 </template>
 
@@ -36,32 +35,16 @@ export default{
 	},
 	created () {
 		// console.log(this.cityId);
-		this.axios.get('api/api/m/catalogData/list-v4.0?catalogId=2&cityId=1&page=1&pageSize=10&siteId=-1').then(res => {
+		this.axios.get('api/api/m/catalogData/list-v4.0?catalogId=2&cityId='+this.cityId+'&page=1&pageSize=10&siteId=-1').then(res => {
 			this.jxThemeData = res.data.data.doc[2].itemData
 			this.shopData = res.data.data.doc[3].itemData
-
 			this.swiperData = res.data.data.extraData.rocket.reserveList[0].content
 			this.bannerData = res.data.data.doc[1].itemData[0].content
 		},err =>{
 			console.log(err)
 		},'json');
 
-		this.axios.get('./static/data/food/shopList.json')
-		.then(res=>{
-			//console.log(res.data.data.doc[2].itemData);
-			this.jxThemeData = res.data.data.doc[2].itemData
-		},err =>{
-			console.log(err);
-		},'json');
-
-		this.axios.get('./static/data/food/shopList.json')
-		.then(res => {
-			//console.log(res.data.data.doc[3].itemData[5].commentTags);
-			this.shopData = res.data.data.doc[3].itemData;
-		},err =>{
-			console.log(err);
-		},'json')		
-
+		
 	},
 	components: {
 		jxTheme, shopList, Swiper, myFooter
